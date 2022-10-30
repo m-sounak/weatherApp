5e4d927b9e5f1673df14b16f2824e7140545ada0
Sounak Majumder <sounakmajumder472@gmail.com>
2022-10-30 19:58:34 +0530

    HTML v1

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -6,22 +6,25 @@ import json
 
 def index(request):
     if request.method == 'POST':
-        city = request.POST['city']
-        source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=2d10e43f69b199b2fade9f3bfe1ad387').read()
+        try:
+            city = request.POST['city']
+            source = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=2d10e43f69b199b2fade9f3bfe1ad387').read()
 
-        listOfData = json.loads(source)
+            listOfData = json.loads(source)
 
-        data = {
-            "countryCode" : str(listOfData['sys']['country']),
-            "coordinate" : str(listOfData['coord']['lat']) + ", " + str(listOfData['coord']['lon']),
-            "temp" : str(listOfData['main']['temp'] - 273),
-            "pressure" : str(listOfData['main']['pressure']),
-            "humidity" : str(listOfData['main']['humidity']),
-            "main" : str(listOfData['weather'][0]['main']),
-            "description" : str(listOfData['weather'][0]['description']),
-            "icon" : listOfData['weather'][0]['icon'], 
-        }
-        print(data)
+            data = {
+                "countryCode" : str(listOfData['sys']['country']),
+                "coordinate" : str(listOfData['coord']['lat']) + ", " + str(listOfData['coord']['lon']),
+                "temp" : str(listOfData['main']['temp'] - 273),
+                "pressure" : str(listOfData['main']['pressure']),
+                "humidity" : str(listOfData['main']['humidity']),
+                "main" : str(listOfData['weather'][0]['main']),
+                "description" : str(listOfData['weather'][0]['description']),
+                "icon" : listOfData['weather'][0]['icon'], 
+            }
+            print(data)
+        except:
+            data = {}
     else:
         data = {}
 
